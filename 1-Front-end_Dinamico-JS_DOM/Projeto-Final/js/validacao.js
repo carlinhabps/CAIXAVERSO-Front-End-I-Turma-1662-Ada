a0fa3027e6cf0bd0bde51e1769b3ebfc6d87cd5f
Cala Beatriz <carlinhabps@hotmail.com>
2026-03-29 15:25:12 -0300

    criação das variáveis e validação dos formulários vazios - cliente e contas

---
+++ 1-Front-end_Dinamico-JS_DOM/Projeto-Final/js/validacao.js
@@ -0,0 +1,81 @@
+// ! validações
+
+const doc = (select) => document.querySelector(select);
+
+// id
+const homeLogo = doc("#homeLogo");
+const navClients = doc("#navClients");
+const navAccounts = doc("#navAccounts");
+const navTransactions = doc("#navTransactions");
+const containerWelcome = doc("#containerWelcome");
+const titleH1 = doc("#titleH1");
+const btnClients = doc("#btnClients");
+const btnAccounts = doc("#btnAccounts");
+const btnTransactions = doc("#btnTransactions");
+const containerClients = doc("#containerClients");
+const inputClientName = doc("#inputClientName");
+const inputClientCpf = doc("#inputClientCpf");
+const inputClientEmail = doc("#inputClientEmail");
+const newClientForm = doc("#newClientForm");
+const btnRegisterNewClient = doc("#btnRegisterNewClient");
+const btnConsultClient = doc("#btnConsultClient");
+const btnEditClient = doc("#btnEditClient");
+const btnDeleteClient = doc("#btnDeleteClient");
+const containerAccounts = doc("#containerAccounts");
+const selectAccountClient = doc("#selectAccountClient");
+const selectAccountType = doc("#selectAccountType");
+const selectAccountStatus = doc("#selectAccountStatus");
+const newAccountForm = doc("#newAccountForm");
+const btnRegisterAccount = doc("#btnRegisterAccount");
+const btnConsultAccount = doc("#btnConsultAccount");
+const btnEditAccount = doc("#btnEditAccount");
+const btnDeleteAccount = doc("#btnDeleteAccount");
+const containerTransactions = doc("#containerTransactions");
+const selectClientName = doc("#selectClientName");
+const selectClientAccount = doc("#selectClientAccount");
+const transactionsList = doc("#transactionsList");
+
+// classes
+const errorMensageClient = doc(".errorMensageClient");
+const errorMensageAccount = doc(".errorMensageAccount");
+const errorMensageTransaction = doc(".errorMensageTransaction");
+
+// Funções
+
+newClientForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const name = inputClientName.value;
+  const cpf = inputClientCpf.value;
+  const email = inputClientEmail.value;
+
+  if (name === "" || cpf === "" || email === "") {
+    errorMensageClient.innerText = "Há campo obrigatório não preenchido!";
+    errorMensageClient.classList.remove("hiddenContent");
+    errorMensageClient.classList.add("errorMensageClient");
+    return;
+  }
+
+  errorMensageClient.classList.add("hiddenContent");
+
+  newClientForm.reset();
+});
+
+newAccountForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const cliente = selectAccountClient.value;
+  const tipo = selectAccountType.value;
+  const satatus = selectAccountStatus.value;
+
+  if (cliente === "defaul" || tipo === "defaul" || satatus === "defaul") {
+    errorMensageAccount.innerText = "Há campo obrigatório não preenchido!";
+    errorMensageAccount.classList.remove("hiddenContent");
+    errorMensageAccount.classList.add("errorMensageAccount");
+    return;
+  }
+
+  errorMensageAccount.classList.add("hiddenContent");
+
+  newAccountForm.reset();
+});
