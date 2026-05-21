55810b444283bead32d6f5e45c0d96f4644e7515
Cala Beatriz <carlinhabps@hotmail.com>
2026-05-21 01:33:01 -0300

    inclusão do calendário e selector do angular material no filtro e novo registro

--- 3-Angular-I/Projeto_Final_Angular/src/app/components/filter/filter.ts
+++ 3-Angular-I/Projeto_Final_Angular/src/app/components/filter/filter.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TIPO } from '../../app';
 import { TransactionService } from '../../service/transaction.service';
 import { CategoryService } from '../../service/category.service';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+
+interface Pokemon {
+  value: string;
+  viewValue: string;
+}
+
+interface PokemonGroup {
+  disabled?: boolean;
+  name: string;
+  pokemon: Pokemon[];
+}
 
 @Component({
   selector: 'app-filter',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatMomentDateModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './filter.html',
   styleUrl: './filter.css',
 })
@@ -20,4 +48,40 @@ export class Filter {
   tipoDespesa = TIPO.DESPESA;
 
   // categoryFilterList = [...this._];
+
+  pokemonControl = new FormControl('');
+  pokemonGroups: PokemonGroup[] = [
+    {
+      name: 'Grass',
+      pokemon: [
+        { value: 'bulbasaur-0', viewValue: 'Bulbasaur' },
+        { value: 'oddish-1', viewValue: 'Oddish' },
+        { value: 'bellsprout-2', viewValue: 'Bellsprout' },
+      ],
+    },
+    {
+      name: 'Water',
+      pokemon: [
+        { value: 'squirtle-3', viewValue: 'Squirtle' },
+        { value: 'psyduck-4', viewValue: 'Psyduck' },
+        { value: 'horsea-5', viewValue: 'Horsea' },
+      ],
+    },
+    {
+      name: 'Fire',
+      disabled: true,
+      pokemon: [
+        { value: 'charmander-6', viewValue: 'Charmander' },
+        { value: 'vulpix-7', viewValue: 'Vulpix' },
+        { value: 'flareon-8', viewValue: 'Flareon' },
+      ],
+    },
+    {
+      name: 'Psychic',
+      pokemon: [
+        { value: 'mew-9', viewValue: 'Mew' },
+        { value: 'mewtwo-10', viewValue: 'Mewtwo' },
+      ],
+    },
+  ];
 }
--- 3-Angular-I/Projeto_Final_Angular/src/app/components/new-register/new-register.ts
+++ 3-Angular-I/Projeto_Final_Angular/src/app/components/new-register/new-register.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-new-register',
-  imports: [FormsModule],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatIconModule],
   templateUrl: './new-register.html',
   styleUrl: './new-register.css',
 })
 export class NewRegister {
-  
+  value: any;
 }
